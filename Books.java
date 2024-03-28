e84ab34f0f90016de16109ac8a73edbe53e6eb3f
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-03-28 14:14:12 +0400

    Develop a program that demonstrates the use of constructors in a class named Book, initializing attributes like title, author, and publicationYear.

---
+++ Books.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Books {
+
+    public String title;
+    public String author;
+    public Integer publicationYear;
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
+

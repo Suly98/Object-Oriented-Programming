8e8272f8ea344c7d57b256613ffa5ec168483dd6
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-03-28 14:55:12 +0400

    Implement a class called Circle with private attributes for radius and color. Include a method to calculate the area of the circle.

---
+++ Circle.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Circle {
+
+    private Float radius;
+    private String color;
+
+    public Float getRadius(){
+        return radius;
+    }
+
+    public void setRadius(){
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Circle(Float radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a Radius: ");
+        Circle circle = new Circle(sc.nextFloat(), "Green");
+
+        Double area= Math.PI* Math.pow(circle.radius, 2);
+        System.out.println("Th");
+
+    }
+}
+

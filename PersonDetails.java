a0cb8b4caffb3bb2a288be8052064da7485b5f21
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-03-28 15:06:19 +0400

    added the attributes

--- PersonDetails.java
+++ PersonDetails.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class PersonDetails {
 
 
+    private String firstName;
+    private String lastName;
+    
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);

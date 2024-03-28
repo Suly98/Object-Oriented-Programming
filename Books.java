7de9fe90e00c41ad252d36b18db7c6930545efcc
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-03-28 14:43:10 +0400

    added the constructors

--- Books.java
+++ Books.java
@@ -6,9 +6,27 @@ public class Books {
     public String author;
     public Integer publicationYear;
 
+    public Books(String title, String author, Integer publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Integer getPublicationYear() {
+        return publicationYear;
+    }
+
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        
 
     }
 }

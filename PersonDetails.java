c0a722caa6f9006a78d13020f244b03bb6592898
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-03-28 15:08:46 +0400

    Write a Java class named PersonDetails with private attributes for firstName and lastName. Include a method to concatenate and return the full name.

--- PersonDetails.java
+++ PersonDetails.java
@@ -5,11 +5,35 @@ public class PersonDetails {
 
     private String firstName;
     private String lastName;
-    
+
+    public PersonDetails(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        PersonDetails person = new PersonDetails("Sulaiman", "Al Hudaifi");
+
+        System.out.println("The person's name is: " + person.firstName);
+        System.out.println("The person's last name is: " + person.lastName);
 
     }
 }
